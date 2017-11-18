7fc985452058d08854dd6902050e52b5eaeea26b
aurel <aurel@192.168.1.12>
2017-11-18 16:33:47 +0100

    test update image

---
+++ Facetagram/src/java/fr/facetagram/etu/user/FileUploadView.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.facetagram.etu.user;
+ 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import org.apache.commons.io.FilenameUtils;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import org.primefaces.model.UploadedFile;
+ 
+@Named(value = "fileUploadView")
+@ApplicationScoped
+public class FileUploadView {
+     
+    private UploadedFile file;
+ 
+    public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+     
+    public void upload() throws IOException {
+        String path= System.getProperty("user.dir"); 
+        FacesMessage message = new FacesMessage("Succesful", path + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        /*
+        if(file != null) {
+            InputStream input = file.getInputstream();
+            Path folder = Paths.get(path);
+            String filename = FilenameUtils.getBaseName(file.getFileName()); 
+            String extension = FilenameUtils.getExtension(file.getFileName());
+            Path file2 = Files.createTempFile(folder, filename + "-", "." + extension);
+            try (InputStream input2 = file.getInputstream()) {
+                Files.copy(input2, file2, StandardCopyOption.REPLACE_EXISTING);
+            }
+            
+        }*/
+    }
+}
