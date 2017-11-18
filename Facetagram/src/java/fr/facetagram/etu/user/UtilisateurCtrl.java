8c8ad687da9c53af7da94a9bdd464260c5ee64cb
Unknown <lhommeletseverin@gmail.com>
2017-11-18 17:09:38 +0100

    Revert "add login"
    This reverts commit da6c2edbce31a0ea9ef919e8078558b8ed7198ac.

--- Facetagram/src/java/fr/facetagram/etu/user/UtilisateurCtrl.java
+++ Facetagram/src/java/fr/facetagram/etu/user/UtilisateurCtrl.java
@@ -5,11 +5,8 @@
  */
 package fr.facetagram.etu.user;
 
-import java.awt.event.ActionEvent;
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.ApplicationScoped;
@@ -20,7 +17,6 @@ import javax.faces.context.FacesContext;
  *
  * @author aurel
  */
-
 @Named(value = "utilisateurCtrl")
 @ApplicationScoped
 public class UtilisateurCtrl implements Serializable {
@@ -31,13 +27,10 @@ public class UtilisateurCtrl implements Serializable {
     private Utilisateur utilisateur;
     
     private Utilisateur selectedUser;
-    
-    private Boolean connecte;
 
     /**
      * Creates a new instance of UtilisateurCtrl
      */
-    
     public UtilisateurCtrl() {
         this.utilisateur = new Utilisateur();
         this.selectedUser = new Utilisateur();
@@ -68,45 +61,5 @@ public class UtilisateurCtrl implements Serializable {
         this.utilisateur = new Utilisateur();
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Etudiant ajouté !"));
     }
-
-    public UtilisateurDAO getDaoUtilisateur() {
-        return daoUtilisateur;
-    }
-
-    public void setDaoUtilisateur(UtilisateurDAO daoUtilisateur) {
-        this.daoUtilisateur = daoUtilisateur;
-    }
-
-    public Boolean getConnecte() {
-        return connecte;
-    }
-
-    public void setConnecte(Boolean connecte) {
-        this.connecte = connecte;
-    }
-    
-     
-     
-     public void login(ActionEvent event) throws IOException {
-        List<Utilisateur> utilisateurs = getUtilisateurs();
-     
-        for(Utilisateur u : utilisateurs){
-            if(u.getEmail().equals(utilisateur.getEmail()) && u.getMotDePasse().equals(utilisateur.getMotDePasse()) ){
-                connecte = true;
-                FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
-                utilisateur = new Utilisateur();
-            }
-        }
-
-    }
-    
-    public void register(ActionEvent event) throws IOException {
-        
-        if(utilisateur.getEmail() != null && utilisateur.getMotDePasse()!= null) {
-            connecte = true;
-            addUtilisateur();
-            FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
-        }
-    } 
     
 }
